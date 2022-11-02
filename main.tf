553d05aa684ec3992cdce3e8d7b184a77485f41b
Andrew Jones <andrew.jones@puppet.com>
2022-11-02 17:00:07 +0000

    test

--- main.tf
+++ main.tf
@@ -134,6 +134,6 @@ resource "aws_instance" "web-server-instance" {
   }
 }
 
-output "server_public_ip" {
-    value = aws_eip.one.public_ip 
-}
+#output "server_public_ip" {
+#    value = aws_eip.one.public_ip 
+#}
