53aca80931ecda881be1388e6f337bc1e92f8f60
Andrew Jones <andrew.jones@puppet.com>
2022-11-02 17:04:05 +0000

    test

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 # 1. Create vpc
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
 
-  tags = { 
-      Name = "production"
+  tags = {
+    Name = "production"
   }
 }
 
@@ -26,8 +26,8 @@ resource "aws_route_table" "prod-route-table" {
   }
 
   route {
-    ipv6_cidr_block        = "::/0"
-    gateway_id = aws_internet_gateway.gw.id
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
   }
 
   tags = {
@@ -37,13 +37,13 @@ resource "aws_route_table" "prod-route-table" {
 
 # 4. Create a Subnet
 resource "aws_subnet" "subnet-1" {
-    vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "eu-west-1a"
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-1a"
 
-    tags = {
-        Name = "prod-subnet"
-    }
+  tags = {
+    Name = "prod-subnet"
+  }
 }
 
 # 5. Associate Subnet with Routed Table
@@ -59,34 +59,34 @@ resource "aws_security_group" "allow_web" {
   vpc_id      = aws_vpc.prod-vpc.id
 
   ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -110,13 +110,13 @@ resource "aws_eip" "one" {
 
 # 9. Create Ubuntu server and install/enable apache2
 resource "aws_instance" "web-server-instance" {
-  ami           = "ami-07d8796a2b0f8d29c"
-  instance_type = "t2.micro"
+  ami               = "ami-07d8796a2b0f8d29c"
+  instance_type     = "t2.micro"
   availability_zone = "eu-west-1a"
-  key_name = "andrew.jones"
+  key_name          = "andrew.jones"
 
   network_interface {
-    device_index = 0
+    device_index         = 0
     network_interface_id = aws_network_interface.web-server-nic.id
   }
 
@@ -129,7 +129,7 @@ resource "aws_instance" "web-server-instance" {
               EOF
 
   tags = {
-    Name = "ubuntu-web"
+    Name     = "ubuntu-web"
     lifetime = "1d"
   }
 }
